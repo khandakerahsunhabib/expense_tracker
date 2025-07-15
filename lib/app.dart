0984167ecba49e8f35b7af5d8516af72330d63a3
khandakerahsunhabib <ahsunhabib96bd@gmail.com>
2025-07-15 21:32:37 +0600

    Updated App, App Colors and App Theme Class

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/core/theme/app_theme.dart';
 import 'package:expense_tracker/main_bottom_nav_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +9,10 @@ class ExpenseTracker extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: "Expense Tracker",
+      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
       home: MainBottomNavScreen(),
     );
   }
---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFF4F378A);
+  static const Color primaryLight = Color(0xFF65558F);
+}
